bff43ec682c99643003bc6621a103c3d08fe6e9f
Nazarbek Altybay <nazarbek.altybay@gmail.com>
2023-04-07 21:27:11 +0600

    Initial commit

---
+++ kilo.cpp
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <termios.h>
+#include <cstdlib>
+
+termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	termios raw = orig_termios;
+
+	raw.c_lflag &= ~(ECHO | ICANON);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+	enableRawMode();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	return 0;
+}
